a5b75d2f15efc648b18e73910e11da228ef78d5f
Marco Antonio <mkchoque@gmail.com>
2022-10-22 15:59:42 -0400

    servidor express básico

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("My App");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res)=>{
+  res.send(`Hola desde mi server en express en el puerto ${port}`);
+});
+
+app.listen(port, ()=>{
+  console.log(`Hola desde consola de express en el puerto ${port}`);
+});
