f3256547e079b4206170f4c7e42b9a16d8019192
Marco Antonio <mkchoque@gmail.com>
2022-10-22 20:34:31 -0400

    rutas express

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res)=>{
   res.send(`Hola desde mi server en express en el puerto ${port}`);
 });
 
+app.get('/nueva-ruta', (req, res)=>{
+  res.send(`Hola desde la nueva ruta`);
+});
+
+app.get('/products', (req, res)=>{
+  res.json({
+    name: 'Producto 1',
+    price: 1000
+  });
+});
+
+
 app.listen(port, ()=>{
   console.log(`Hola desde consola de express en el puerto ${port}`);
 });
